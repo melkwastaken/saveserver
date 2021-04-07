054ce5fe7faf87a1e177cbc3c2056a196406b835
melkwastaken <77582036+melkwastaken@users.noreply.github.com>
2021-04-07 04:56:02 +0100

    Create save.cs

---
+++ save.cs
@@ -0,0 +1,143 @@
+using System;
+
+namespace System
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("crack crack crack");    
+    }
+  }
+}
+
+{
+static void main(System[] WriteLine)
+    }
+  }
+}
+
+int myNum = 5;               // Integer (whole number)
+double myDoubleNum = 5.99D;  // Floating point number
+char myLetter = 'D';         // Character
+bool myBool = true;          // Boolean
+string myText = "Hello";     // String
+
+int myInt = 9;
+double myDouble = myInt;       // Automatic casting: int to double
+
+Console.WriteLine(myInt);      // Outputs 9
+Console.WriteLine(myDouble);   // Outputs 9
+int x = 100 + 50;
+
+Math.Max(5, 10);
+
+string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+bool isCSharpFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCSharpFun);   // Outputs True
+Console.WriteLine(isFishTasty);   // Outputs False
+
+if (condition) 
+{
+  // block of code to be executed if the condition is True
+}
+
+switch(expression) 
+{
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+    break;
+}
+
+for (statement 1; statement 2; statement 3) 
+{
+  // code block to be executed
+}
+
+for (int i = 0; i < 10; i++) 
+{
+  if (i == 4) 
+  {
+    break;
+  }
+  Console.WriteLine(i);
+}
+
+string[] cars;
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+int[] myNum = {10, 20, 30, 40};
+
+
+class Program
+{
+  static void MyMethod() 
+  {
+    // code to be executed
+    static void MyMethod(string fname) 
+{
+  Console.WriteLine(fname + " Refsnes");
+}
+
+static void Main(string[] args)
+{
+  MyMethod("Liam");
+  MyMethod("Jenny");
+  MyMethod("Anja");
+}
+
+// Liam Refsnes
+// Jenny Refsnes
+// Anja Refsnes
+
+int MyMethod(int x)
+float MyMethod(float x)
+double MyMethod(double x, double y)
+
+static int PlusMethodInt(int x, int y)
+{
+  return x + y;
+}
+
+static double PlusMethodDouble(double x, double y)
+{
+  return x + y;
+}
+
+static void Main(string[] args)
+{
+  int myNum1 = PlusMethodInt(8, 5);
+  double myNum2 = PlusMethodDouble(4.3, 6.26);
+  Console.WriteLine("Int: " + myNum1);
+  Console.WriteLine("Double: " + myNum2);
+}
+static int PlusMethod(int x, int y)
+{
+  return x + y;
+}
+
+static double PlusMethod(double x, double y)
+{
+  return x + y;
+}
+
+static void Main(string[] args)
+{
+  int myNum1 = PlusMethod(8, 5);
+  double myNum2 = PlusMethod(4.3, 6.26);
+  Console.WriteLine("Int: " + myNum1);
+  Console.WriteLine("Double: " + myNum2);
+}
+
+  }
+}
