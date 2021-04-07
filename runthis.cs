a6c29783d928d601c9e43bd1ed537b7a81dab658
melkwastaken <77582036+melkwastaken@users.noreply.github.com>
2021-04-07 05:00:52 +0100

    fucking failure codeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ runthis.cs
@@ -0,0 +1,16 @@
+using System
+
+Namespace System
+
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Saving server...");
+      console.WriteLine("Ran to a error.")
+      console.WriteLine("ahhahaha")
+      args()
+    }
+  }
+}
